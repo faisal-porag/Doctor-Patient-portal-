8e233c0f95320cf7a5e1a030217623432c5eb784
Faisal-CSE <porag_faisal123@hotmail.com>
2017-02-03 11:24:08 +0600

    Add files via upload

---
+++ db_conn.php
@@ -0,0 +1,18 @@
+ <?php
+	function get_db_connection(){
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "doctorpatientportal";
+
+		// Create connection
+		$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+		// Check connection
+		if (!$conn) {
+		    die("Ops! Something wrong has happened!" . mysqli_connect_error());
+		}
+		
+		return $conn;
+	}
+?>
